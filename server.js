b49ecb8c4644bac1de0615e835b0f8ee3e6597d3
Asif <asraf2asif@gmail.com>
2022-12-20 22:32:23 +0600

    Update server.js

--- server.js
+++ server.js
@@ -37,17 +37,21 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirName = path.resolve();
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirName, '/frontend/build')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirName, '/frontend/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirName, 'frontend', 'build', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirName, 'frontend', 'build', 'index.html'))
+//   );
+// } else {
+//   app.get('/', (req, res) => {
+//     res.send('API is running....');
+//   });
+// }
+
+app.get('/', (req, res) => {
+  res.send('API is running....');
+});
 
 app.use(handleErrors);
 
